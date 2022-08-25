703aedbf699fabdca30b07ccb5afd89da6d88da0
AnkitaSMusale <ankitamusale4@gmail.com>
2022-08-25 23:35:11 +0530

    e-commerse website Frontend with Order Feature

--- E-commerse/cart.js
+++ E-commerse/cart.js
@@ -27,23 +27,25 @@ window.addEventListener('DOMContentLoaded', () => {
 function addToCart(productId) {
   axios.post("http://localhost:7000/cart", { productId: productId })
     .then((response) => {
-      // if (response.status === 200) {
-      //   console.log(response);
-
-      //   notifyuser(response.data.message);
-      // }
-       if (!(document.querySelectorAll(`#in-cart-${productId}`))) {
+      if (response.status === 200) {
+         console.log(response);
+         document.location.reload()
         notifyuser(response.data.message);
-       }
-       else{
-        alert('This item is already added to the cart');
-         return;
-        
-       }
+      }
+      console.log(document.querySelectorAll(`#in-cart-${productId}`))
+      if ((document.querySelectorAll(`#in-cart-${productId}`))) {
+        notifyuser(response.data.message);
+      }
+      // else {
+      //   alert('This item is already added to the cart');
+      //   return;
+
+      // }
+
     })
     .catch((err) => {
       console.log(err);
-      notifyuser(err.data.message);
+      notifyuser(err);
     });
 }
 
@@ -57,7 +59,7 @@ function notifyuser(message) {
 
   setTimeout(() => {
     notify.remove();
-  }, 3000);
+  }, 2000);
 }
 // ********************************************** //
 const hamburgerbtn = document.getElementById("hamburgerbtn");
@@ -138,10 +140,10 @@ parentContainer.addEventListener('click', (e) => {
     const name = document.querySelector(`#${id} h3`).innerText;
     const img_src = document.querySelector(`#${id} img`).src;
     const price = document.querySelector(`#${id} h4`).innerText;
-    if (!(document.querySelector(`#in-cart-${id}`))) {
+    if ((document.querySelector(`#in-cart-${id}`))) {
       createNotification();
     }
-    if (document.querySelector(`#in-cart-${id}`)) {
+    if (document.querySelector(`#in-cart-${product.id}`)) {
       alert('This item is already added to the cart');
       return
     }
@@ -154,16 +156,21 @@ parentContainer.addEventListener('click', (e) => {
         <div class="cart_prod_title">${name} </div>
         <div class="cart_prod_price">${price}</div>
         <input type="number" value="1" class="cartquantity">
+        
+        </div>
+        <div class="removetemp" id="removetemp"> 
+            <button onClick="RemoveFromCart({${id})" id="removebtn" class="removebtn"><strong>X</strong></button>
         </div>
         `;
     cart_items.appendChild(cart_item);
-  } 
-  updatetotal();
-  var quantityInput = document.getElementsByClassName('cartquantity');
-  for (var i = 0; quantityInput.length; i++) {
-    var input = quantityInput[i];
-    input.addEventListener('change', quantityChanged);
+    window.reload();
   }
+  updatetotal();
+  // var quantityInput = document.getElementsByClassName('cartquantity');
+  // for (var i = 0; quantityInput.length; i++) {
+  //   var input = quantityInput[i];
+  //   input.addEventListener('change', quantityChanged); 
+  // }
 })
 
 function quantityChanged(event) {
@@ -190,6 +197,164 @@ function createNotification() {
 // *************************Show Cart   ******************************** //
 window.addEventListener('DOMContentLoaded', () => {
   axios.get("http://localhost:7000/cart")
+    .then((data) => {
+      if (data.request.status === 200) {
+        const products = data.data.products;
+        console.log(products);
+        //console.log(data);
+        const parentSecton = document.getElementById('cart_content');
+        products.forEach(product => {
+          const productHTML = `
+                <div id="${product.id}" class="cartbox">
+                    <img src="${product.imageUrl}" alt="" class="cartimg">
+                    <div class="detailbox">
+                    <div class="cart_prod_title">${product.title} </div>
+                    <div class="cart_prod_price">$${product.price}</div>
+                    <input type="number" value="1" class="cartquantity">
+                    </div>
+                    <div class="remove" id="remove"> 
+                    <button onClick="RemoveFromCart(event, ${product.id})" id="removebtn" class="removebtn"><strong>X</strong></button>
+                    </div>    
+                </div>
+                  `;
+          parentSecton.innerHTML += productHTML;
+
+        });
+        updatetotal();
+        var quantityInput = document.getElementsByClassName('cartquantity');
+        for (var i = 0; quantityInput.length; i++) {
+          var input = quantityInput[i];
+          input.addEventListener('change', quantityChanged);
+        }
+      }
+    })
+    .catch(err => console.log(err));
+})
+
+function RemoveFromCart(e,prodId){
+  e.preventDefault();
+  const data = confirm('Do you want to delete this item ?')
+        if(data == true){
+            const liId=e.target.parentNode.parentNode.parentNode.id;
+            console.log(liId);
+            const li=e.target.parentNode.parentNode.parentNode;
+            console.log(li)
+            li.remove()
+            //document.location.reload();
+            axios.post(`http://localhost:7000/cart-delete-item`,{productId:prodId})
+           .then(()=> {
+             li.remove()
+            })
+            .catch(err=>{console.log(err)})
+        }  
+        else{
+             return false; 
+        } 
+}
+
+// function RemoveFromCartFrontend(prodId){
+//   //const prod = document.getElementById(prodId)
+//   // window.reload();
+//   // console.log(cart_items)
+//   // cart_items.removeChild(prodId);
+  
+// }
+
+// cartlist=document.getElementById('cart_content')
+// cartlist.addEventListener('click',(e)=>{
+//     if(e.target.id=='removebtn'){
+//         const data = confirm('Do you want to delete this item ?')
+//         if(data == true){
+//             const liId=e.target.parentNode.id;
+//             console.log(liId);
+//             const li=e.target.parentNode;
+//             console.log(li)
+//             li.remove()
+//             axios.post(`http://localhost:3000/cart-delete-item/${liId}`)
+//             .then((res)=>{
+//                 console.log(res);
+//             })
+//             .catch(err=>{console.log(err)})
+//         }  
+//         else{
+//              return false; 
+//         } 
+//     }        
+// })
+
+
+//Order place
+const order = document.getElementById('purchase-btn2');
+order.addEventListener('click', () => {
+  axios.post('http://localhost:7000/order-place')
+  .then((response) => {
+    if (response.status === 200) {
+      console.log(response);
+      notifyuser(response.data.message);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+    //notifyuser(err);
+  })
+})
+
+// ************************Get orders from backend*************************************
+window.addEventListener('DOMContentLoaded', () => {
+  axios.get("http://localhost:7000/orders")
+    .then((data) => {
+      if (data.request.status === 200) {
+        const products = data.data;
+        console.log(products);
+        //console.log(data);
+    //     const parentSecton = document.getElementById('cart_content');
+    //     products.forEach(product => {
+    //       const productHTML = `
+    //             <div id="${product.id}" class="cartbox">
+    //                 <img src="${product.imageUrl}" alt="" class="cartimg">
+    //                 <div class="detailbox">
+    //                 <div class="cart_prod_title">${product.title} </div>
+    //                 <div class="cart_prod_price">$${product.price}</div>
+    //                 <input type="number" value="1" class="cartquantity">
+    //                 </div>
+    //                 <div class="remove" id="remove"> 
+    //                 <button onClick="RemoveFromCart(event, ${product.id})" id="removebtn" class="removebtn"><strong>X</strong></button>
+    //                 </div>    
+    //             </div>
+    //               `;
+    //       parentSecton.innerHTML += productHTML;
+
+    //     });
+    //     updatetotal();
+    //     var quantityInput = document.getElementsByClassName('cartquantity');
+    //     for (var i = 0; quantityInput.length; i++) {
+    //       var input = quantityInput[i];
+    //       input.addEventListener('change', quantityChanged);
+    //     }
+       }
+    })
+    .catch(err => console.log(err));
+})
+// ************************************************************
+  
+  // if (parseInt(document.querySelector('cartbox').innerText) === 0){
+  //     alert('You have Nothing in Cart , Add some products to purchase !');
+  // }
+  // else{
+  //     axios.post('http://localhost:7000/order-place')
+  //     .then((response)=>{
+  //         notifyuser(response.data.message);
+  //         const cart_items = document.querySelector('#cart .cart-items');
+  //         cart_items.innerHTML = "";
+  //         document.querySelector('cartbox').innerText = '0'
+  //         document.querySelector('#total-price').innerText = '0';
+  //     })
+  // }
+
+//****************** */
+/*
+window.addEventListener('DOMContentLoaded', () => {
+  axios.get("http://localhost:7000/product?page=1")
   .then((data) => {
           if (data.request.status === 200) {
             const products = data.data.products;
@@ -204,11 +369,11 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="cart_prod_title">${product.title} </div>
                     <div class="cart_prod_price">$${product.price}</div>
                     <input type="number" value="1" class="cartquantity">
-                    </div>     
+                    </div>
                 </div>
                   `;
               parentSecton.innerHTML += productHTML;
-    
+
             });
             updatetotal();
             var quantityInput = document.getElementsByClassName('cartquantity');
@@ -219,7 +384,7 @@ window.addEventListener('DOMContentLoaded', () => {
           }
         })
         .catch(err => console.log(err));
-})
+}) */
 // const cartbutton = document.getElementById('hamburgerbtn');
 // cartbutton.addEventListener('click', (e) => {
 //   axios.get("http://localhost:7000/cart")
@@ -237,7 +402,7 @@ window.addEventListener('DOMContentLoaded', () => {
 //                 <div class="cart_prod_title">${product.title} </div>
 //                 <div class="cart_prod_price">$${product.price}</div>
 //                 <input type="number" value="1" class="cartquantity">
-//                 </div>     
+//                 </div>
 //             </div>
 //               `;
 //           parentSecton.innerHTML += productHTML;
@@ -256,132 +421,3 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-
-
-
-
-
-
-// ******************************************************* //
-//var addCart = document.getElementsByClassName('shop-item-button');
-/* var addCart = document.getElementById('btn1');
-for(var i=0; quantityInput.length; i++){
-  var button = addCart[i];
-  button.addEventListener('click',addcartClicked);
-}
-
-function addcartClicked(event){
-  var button = event.target;
-  var shopProducts = button.parentElement;
-  var title = shopProducts.getElementsByClassName('product__title')[0].innerText;
-  var price = shopProducts.getElementsByClassName('product__price')[0].innerText;
-  var image = shopProducts.getElementsByClassName('product_img')[0].src;
-  console.log(title,price,image);
-  addProductToCart(title,price,image);
-  updatetotal();
-}
-
-function addProductToCart(title,price,image){
-  var cartShopBox = document.createElement("div");
-  cartShopBox.classList.add("cartbox")
-  var cartItems = document.getElementsByClassName("cart_content")[0];
-  var cartItemNames = cartItems.getElementsByClassName("cart_prod_title");
-  for(var i=0; cartItemNames.length; i++){
-    if(cartItemNames[i].innerText == title){
-      alert("You have already added this to cart");
-      return;
-    }
-  }
-  var cartBoxContent = `
-<img src="${image}" alt="" class="cartimg">
-<div class="detailbox">
-    <div class="cart_prod_title">${title} </div>
-    <div class="cart_prod_price">${price}</div>
-    <input type="number" value="1" class="cartquantity">
-</div>
-`;
-
-
-cartShopBox.innerHTML = cartBoxContent;
-cartItems.appendChild(cartShopBox);
-cartShopBox.getElementsByClassName('')
-}
-
-
-const product = document.getElementsByClassName('shop-item-button');
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//******************************************** /
-/*
-const btn1 = document.getElementById("btn1");
-const container1 = document.getElementById("product1");
-const btn2 = document.getElementById("btn2");
-const container2 = document.getElementById("product2");
-const btn3 = document.getElementById("btn3");
-const container3 = document.getElementById("product3");
-const btn4 = document.getElementById("btn4");
-const container4 = document.getElementById("product4");
-
-const product = document.getElementById("toasting");
-
-btn1.addEventListener('click', () => {
-  //createNotification();
-  //const cart_items = document.getElementById("cart-items");
-  const product1 = document.getElementById('product1');
-  //const title = document.getElementById('product__title');
-  //const img = document.getElementById('img');
-  //const price = document.getElementById('product__price');
-  //const cart_item = document.createElement('itemlist');
-  console.log(product1);
-  //const childHTML = `<li = <span> ${product1.title} <img class='img' src="${product1.img}"> ${product1.price} </span>>`;
-  //cart_item.innerHTML += childHTML;
-  
-  
-  
-})
-btn2.addEventListener('click', () => {
-    createNotification();
-})
-btn3.addEventListener('click', () => {
-    createNotification();
-})
-btn4.addEventListener('click', () => {
-    createNotification();
-})  
-
-
-
-
-const cart_items = document.getElementById("cart-items")
-const parentNode = document.getElementById('productlist');
-function addtocart(){
-  const id = document.getElementById('id');
-  const title = document.getElementById('product__title');
-  const img = document.getElementById('img');
-  const price = document.getElementById('product__price');
-  const cart_item = document.createElement('itemlist');
-
-  
-  cart_item.innerHTML = `<span> ${title} <img class='img' src="${img}"> ${price} </span>`;
-  cart_items.appendChild(cart_item);
-
-  
-    //const childHTML = `<li id=${userdetail.id}> ${userdetail.title} ${userdetail.img}>`;
-    //cart_items.innerHTML += childHTML;
-
-}
-*/
